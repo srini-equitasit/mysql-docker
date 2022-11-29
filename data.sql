a65afb9dca5db5238ceab2e0b907d7dbd8e8f76b
srini-equitasit <117010387+srini-equitasit@users.noreply.github.com>
2022-11-29 19:35:43 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,7 @@
-insert into DEPT (DEPTNO, DNAME, LOC) values(10, 'ACCOUNTING', 'NEW YORK')
+-- department data ---
+insert into DEPT (DEPTNO, DNAME, LOC) values(10, 'ACCOUNTING', 'NEW YORK');
+insert into DEPT (DEPTNO, DNAME, LOC) values(20, 'RESEARCH', 'DALLAS');
+insert into DEPT (DEPTNO, DNAME, LOC) values(30, 'SALES', 'CHICAGO');
+insert into DEPT (DEPTNO, DNAME, LOC) values(40, 'OPERATIONS', 'BOSTON');
+----------
+---emp data----
